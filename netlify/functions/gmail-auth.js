2e8bec214789a0567bb9453fa30fa3975e45b2af
vishal <vishalranka90@gmail.com>
2026-06-12 05:55:58 -0400

    fix: single email provider (Gmail hides Outlook and vice versa) + fix Gmail email display scope

--- netlify/functions/gmail-auth.js
+++ netlify/functions/gmail-auth.js
@@ -4,7 +4,9 @@ exports.handler = async function(event) {
 
   const scopes = [
     'https://www.googleapis.com/auth/gmail.readonly',
-    'https://www.googleapis.com/auth/calendar.readonly'
+    'https://www.googleapis.com/auth/calendar.readonly',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
   ].join(' ');
 
   const authUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' +
